eae5c47b81bb4b3c760737bc3eed7f20bfca5f7b
Boris <bdumontet@telecomdesign.fr>
2016-01-27 15:25:15 +0000

    Change language in contact ajax request

--- js/contact.js
+++ js/contact.js
@@ -35,7 +35,7 @@ $(function() {
                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                        .append("</button>");
                    $('#success > .alert-success')
-                       .append("<strong>Votre message a bien été envoyé. Nous vous répondrons dans les plus brefs délais.</strong>");
+                       .append("<strong>Your message has been sent. We will reply to your enquiry as soon as possible.</strong>");
                    $('#success > .alert-success')
                        .append('</div>');
 
@@ -47,7 +47,7 @@ $(function() {
                    $('#success').html("<div class='alert alert-danger'>");
                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                        .append("</button>");
-                   $('#success > .alert-danger').append("<strong>Désolé, le serveur ne semble pas répondre. Ré-essayez ultérieurement.");
+                   $('#success > .alert-danger').append("<strong>Sorry, the server is unavailable or responding not as expected. Please re-try later.");
                    $('#success > .alert-danger').append('</div>');
                    //clear all fields
                    $('#contactForm').trigger("reset");
